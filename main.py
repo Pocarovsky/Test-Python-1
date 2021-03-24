803d3cb66c5395926878eefb8e2fecbe51c8d03e
Tomas Pocarovsky <tomas.cary@gmail.com>
2021-03-24 19:33:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Priklad poznamky
+name = "Value"
+cislo = 99
+print("Jmeno je :", name)
+print(cislo)
+
+cislo = 99 *2
+print(cislo)
+
+pi = 3.14
+print(pi)
+
+cislo = cislo + 0.1
+print (cislo)
+print (type(cislo))
+print(type(name))
